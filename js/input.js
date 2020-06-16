98ca61d179148028831a44aebf7fc121b7438c14
Bahram Ismayilov <bahram.ismayilov@hotmail.com>
2020-06-16 19:24:05 +0400

    delete finger swiped through javscript

--- js/input.js
+++ js/input.js
@@ -1,76 +1,43 @@
 let inputDirection = {
   x: 0,
-  y: 0
-}
+  y: 0,
+};
 let lastInputDirection = {
   x: 0,
-  y: 0
-}
-window.addEventListener("keydown", k => {
+  y: 0,
+};
+window.addEventListener("keydown", (k) => {
   switch (k.key) {
     case "ArrowUp":
       if (lastInputDirection.y !== 0) break;
       inputDirection = {
         x: 0,
-        y: -1
-      }
-      break
+        y: -1,
+      };
+      break;
     case "ArrowDown":
       if (lastInputDirection.y !== 0) break;
       inputDirection = {
         x: 0,
-        y: 1
-      }
-      break
+        y: 1,
+      };
+      break;
     case "ArrowLeft":
       if (lastInputDirection.x !== 0) break;
       inputDirection = {
         x: -1,
-        y: 0
-      }
-      break
+        y: 0,
+      };
+      break;
     case "ArrowRight":
       if (lastInputDirection.x !== 0) break;
       inputDirection = {
         x: 1,
-        y: 0
-      }
-      break
+        y: 0,
+      };
+      break;
   }
-})
-
-window.addEventListener("swiped-left", k => {
-  console.log(k.target);
-  // if (lastInputDirection.x !== 0) break;
-  inputDirection = {
-    x: -1,
-    y: 0
-  }
-})
-window.addEventListener("swiped-right", k => {
-  console.log(k.target);
-  // if (lastInputDirection.x !== 0) break;
-  inputDirection = {
-    x: 1,
-    y: 0
-  }
-})
-window.addEventListener("swiped-up", k => {
-  console.log(k.target);
-  // if (lastInputDirection.y !== 0) break;
-  inputDirection = {
-    x: 0,
-    y: -1
-  }
-})
-window.addEventListener("swiped-down", k => {
-  console.log(k.target);
-  // if (lastInputDirection.y !== 0) break;
-  inputDirection = {
-    x: 0,
-    y: 1
-  }
-})
+});
 
 export function getInputDirection() {
   lastInputDirection = inputDirection;
